0c9b18116e2385b31d24198cabe4873984670b90
Himanshuyadav11 <91896301+Himanshuyadav11@users.noreply.github.com>
2025-05-30 17:02:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region = ap-south-1
+  access_key = credentials('AWS_ACCESS_KEY_ID')
+  secret_key = credentials('AWS_SECRET_ACCESS_KEY')
+}
+
 resource "aws_instance" "foo" {
   ami           = "ami-0e35ddab05955cf57"
   instance_type = "t2.micro"
